f00b6f10fe1bd67c1c8e8b4a35502776a5f62d52
Nabifr <Husninabilfareno07@gmail.com>
2023-06-07 20:06:13 +0700

    Register

--- app/src/main/java/com/binar/projekakhir/view/ui/RegisterFragment.kt
+++ app/src/main/java/com/binar/projekakhir/view/ui/RegisterFragment.kt
@@ -6,17 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.binar.projekakhir.R
+import com.binar.projekakhir.databinding.FragmentRegisterBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class RegisterFragment : Fragment() {
-
+lateinit var binding : FragmentRegisterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 
